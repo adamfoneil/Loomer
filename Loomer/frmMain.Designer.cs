7db36ad1549a1df24aaaea0521a882c6d7be11f0
Adam O'Neil <adamosoftware@gmail.com>
2019-12-08 17:21:15 -0500

    some layout changes

--- Loomer/frmMain.Designer.cs
+++ Loomer/frmMain.Designer.cs
@@ -35,13 +35,14 @@
             this.label5 = new System.Windows.Forms.Label();
             this.nudSquareSize = new System.Windows.Forms.NumericUpDown();
             this.btnDraw2 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
             this.dgvHarnesses = new System.Windows.Forms.DataGridView();
             this.label3 = new System.Windows.Forms.Label();
             this.cbWeftColor = new System.Windows.Forms.ComboBox();
             this.label2 = new System.Windows.Forms.Label();
             this.cbWarpColor = new System.Windows.Forms.ComboBox();
             this.btnClear = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.colLetter = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colStart = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.colPattern = new System.Windows.Forms.DataGridViewTextBoxColumn();
@@ -50,6 +51,8 @@
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.nudSquareSize)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHarnesses)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -61,19 +64,9 @@
             // 
             // splitContainer1.Panel1
             // 
-            this.splitContainer1.Panel1.Controls.Add(this.label1);
-            this.splitContainer1.Panel1.Controls.Add(this.tbHarnessOrder);
-            this.splitContainer1.Panel1.Controls.Add(this.chkDrawCoordinates);
-            this.splitContainer1.Panel1.Controls.Add(this.label5);
-            this.splitContainer1.Panel1.Controls.Add(this.nudSquareSize);
-            this.splitContainer1.Panel1.Controls.Add(this.btnDraw2);
-            this.splitContainer1.Panel1.Controls.Add(this.label4);
             this.splitContainer1.Panel1.Controls.Add(this.dgvHarnesses);
-            this.splitContainer1.Panel1.Controls.Add(this.label3);
-            this.splitContainer1.Panel1.Controls.Add(this.cbWeftColor);
-            this.splitContainer1.Panel1.Controls.Add(this.label2);
-            this.splitContainer1.Panel1.Controls.Add(this.cbWarpColor);
-            this.splitContainer1.Panel1.Controls.Add(this.btnClear);
+            this.splitContainer1.Panel1.Controls.Add(this.panel2);
+            this.splitContainer1.Panel1.Controls.Add(this.panel1);
             this.splitContainer1.Size = new System.Drawing.Size(800, 497);
             this.splitContainer1.SplitterDistance = 241;
             this.splitContainer1.TabIndex = 0;
@@ -82,7 +75,7 @@
             // 
             this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 366);
+            this.label1.Location = new System.Drawing.Point(12, 12);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(145, 13);
             this.label1.TabIndex = 12;
@@ -91,7 +84,7 @@
             // tbHarnessOrder
             // 
             this.tbHarnessOrder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.tbHarnessOrder.Location = new System.Drawing.Point(12, 382);
+            this.tbHarnessOrder.Location = new System.Drawing.Point(12, 30);
             this.tbHarnessOrder.Name = "tbHarnessOrder";
             this.tbHarnessOrder.Size = new System.Drawing.Size(213, 20);
             this.tbHarnessOrder.TabIndex = 11;
@@ -100,7 +93,7 @@
             // 
             this.chkDrawCoordinates.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.chkDrawCoordinates.AutoSize = true;
-            this.chkDrawCoordinates.Location = new System.Drawing.Point(12, 410);
+            this.chkDrawCoordinates.Location = new System.Drawing.Point(12, 59);
             this.chkDrawCoordinates.Name = "chkDrawCoordinates";
             this.chkDrawCoordinates.Size = new System.Drawing.Size(109, 17);
             this.chkDrawCoordinates.TabIndex = 8;
@@ -110,7 +103,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(14, 68);
+            this.label5.Location = new System.Drawing.Point(12, 69);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(67, 13);
             this.label5.TabIndex = 4;
@@ -118,7 +111,7 @@
             // 
             // nudSquareSize
             // 
-            this.nudSquareSize.Location = new System.Drawing.Point(87, 66);
+            this.nudSquareSize.Location = new System.Drawing.Point(87, 67);
             this.nudSquareSize.Minimum = new decimal(new int[] {
             5,
             0,
@@ -136,7 +129,7 @@
             // btnDraw2
             // 
             this.btnDraw2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnDraw2.Location = new System.Drawing.Point(12, 433);
+            this.btnDraw2.Location = new System.Drawing.Point(12, 82);
             this.btnDraw2.Name = "btnDraw2";
             this.btnDraw2.Size = new System.Drawing.Size(213, 23);
             this.btnDraw2.TabIndex = 9;
@@ -144,36 +137,26 @@
             this.btnDraw2.UseVisualStyleBackColor = true;
             this.btnDraw2.Click += new System.EventHandler(this.btnDraw_Click);
             // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 102);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(60, 13);
-            this.label4.TabIndex = 6;
-            this.label4.Text = "Harnesses:";
-            // 
             // dgvHarnesses
             // 
             this.dgvHarnesses.AllowUserToResizeColumns = false;
             this.dgvHarnesses.AllowUserToResizeRows = false;
-            this.dgvHarnesses.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left)));
             this.dgvHarnesses.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvHarnesses.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.colLetter,
             this.colStart,
             this.colPattern});
-            this.dgvHarnesses.Location = new System.Drawing.Point(15, 118);
+            this.dgvHarnesses.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvHarnesses.Location = new System.Drawing.Point(0, 108);
             this.dgvHarnesses.Name = "dgvHarnesses";
-            this.dgvHarnesses.Size = new System.Drawing.Size(213, 239);
+            this.dgvHarnesses.Size = new System.Drawing.Size(241, 240);
             this.dgvHarnesses.TabIndex = 7;
             this.dgvHarnesses.DefaultValuesNeeded += new System.Windows.Forms.DataGridViewRowEventHandler(this.dgvPatternRules_DefaultValuesNeeded);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 42);
+            this.label3.Location = new System.Drawing.Point(12, 43);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(60, 13);
             this.label3.TabIndex = 2;
@@ -183,7 +166,7 @@
             // 
             this.cbWeftColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbWeftColor.FormattingEnabled = true;
-            this.cbWeftColor.Location = new System.Drawing.Point(87, 39);
+            this.cbWeftColor.Location = new System.Drawing.Point(87, 40);
             this.cbWeftColor.Name = "cbWeftColor";
             this.cbWeftColor.Size = new System.Drawing.Size(138, 21);
             this.cbWeftColor.TabIndex = 3;
@@ -191,7 +174,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 15);
+            this.label2.Location = new System.Drawing.Point(12, 16);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(63, 13);
             this.label2.TabIndex = 0;
@@ -201,7 +184,7 @@
             // 
             this.cbWarpColor.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbWarpColor.FormattingEnabled = true;
-            this.cbWarpColor.Location = new System.Drawing.Point(87, 12);
+            this.cbWarpColor.Location = new System.Drawing.Point(87, 13);
             this.cbWarpColor.Name = "cbWarpColor";
             this.cbWarpColor.Size = new System.Drawing.Size(138, 21);
             this.cbWarpColor.TabIndex = 1;
@@ -209,7 +192,7 @@
             // btnClear
             // 
             this.btnClear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.btnClear.Location = new System.Drawing.Point(12, 462);
+            this.btnClear.Location = new System.Drawing.Point(12, 111);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(213, 23);
             this.btnClear.TabIndex = 10;
@@ -217,20 +200,49 @@
             this.btnClear.UseVisualStyleBackColor = true;
             this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.nudSquareSize);
+            this.panel1.Controls.Add(this.cbWeftColor);
+            this.panel1.Controls.Add(this.cbWarpColor);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(241, 108);
+            this.panel1.TabIndex = 13;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.tbHarnessOrder);
+            this.panel2.Controls.Add(this.btnDraw2);
+            this.panel2.Controls.Add(this.btnClear);
+            this.panel2.Controls.Add(this.chkDrawCoordinates);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 348);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(241, 149);
+            this.panel2.TabIndex = 14;
+            // 
             // colLetter
             // 
-            this.colLetter.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colLetter.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.colLetter.DataPropertyName = "Letter";
             this.colLetter.HeaderText = "Harness";
             this.colLetter.MaxInputLength = 1;
             this.colLetter.Name = "colLetter";
+            this.colLetter.Width = 71;
             // 
             // colStart
             // 
-            this.colStart.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.colStart.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
             this.colStart.DataPropertyName = "StartValue";
             this.colStart.HeaderText = "Start";
             this.colStart.Name = "colStart";
+            this.colStart.Width = 54;
             // 
             // colPattern
             // 
@@ -250,11 +262,14 @@
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             this.splitContainer1.Panel1.ResumeLayout(false);
-            this.splitContainer1.Panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.nudSquareSize)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvHarnesses)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -267,7 +282,6 @@
         private System.Windows.Forms.ComboBox cbWeftColor;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.ComboBox cbWarpColor;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.DataGridView dgvHarnesses;
         private System.Windows.Forms.Button btnDraw2;
         private System.Windows.Forms.Label label5;
@@ -275,6 +289,8 @@
         private System.Windows.Forms.CheckBox chkDrawCoordinates;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tbHarnessOrder;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.DataGridViewTextBoxColumn colLetter;
         private System.Windows.Forms.DataGridViewTextBoxColumn colStart;
         private System.Windows.Forms.DataGridViewTextBoxColumn colPattern;
